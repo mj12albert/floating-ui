0c4a1efd42ef39d5fe0228b5045046d1085554fc
Albert Yu <albert@albertyu.co>
2024-10-10 23:03:41 +0800

    Add test

--- packages/react/test/visual/components/New.tsx
+++ packages/react/test/visual/components/New.tsx
@@ -1,17 +1,42 @@
-import {useFloating} from '@floating-ui/react';
+import {useState} from 'react';
+import {useFloating, useInteractions, useDismiss} from '@floating-ui/react';
 
 export function New() {
-  const {refs, floatingStyles} = useFloating();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const {refs, floatingStyles, context} = useFloating({
+    open: isOpen,
+    onOpenChange: setIsOpen,
+  });
+
+  const dismiss = useDismiss(context);
+
+  const {getReferenceProps, getFloatingProps} = useInteractions([dismiss]);
 
   return (
     <>
-      <h1 className="text-5xl font-bold mb-8">New</h1>
-      <div className="grid place-items-center border border-slate-400 rounded lg:w-[40rem] h-[20rem] mb-4">
-        <div ref={refs.setReference}>Reference</div>
-        <div ref={refs.setFloating} style={floatingStyles}>
-          Floating
+      <a href="https://github.com/floating-ui/floating-ui/pull/3075">
+        <h2>https://github.com/floating-ui/floating-ui/pull/3075</h2>
+      </a>
+      <button
+        ref={refs.setReference}
+        onClick={() => setIsOpen(!isOpen)}
+        className="bg-rose-500 text-white rounded-md"
+        {...getReferenceProps()}
+      >
+        Trigger / Reference element
+      </button>
+
+      {isOpen && (
+        <div
+          ref={refs.setFloating}
+          role="tooltip"
+          style={floatingStyles}
+          {...getFloatingProps()}
+        >
+          <input />
         </div>
-      </div>
+      )}
     </>
   );
 }
